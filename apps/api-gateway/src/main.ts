b439e262d52fa30bd9d0d21a755b8bc207e2afa1
AnilKumt <anilkumt51@gmail.com>
2025-09-02 21:45:42 +0530

    setup all the required middleware for parsing, rate limiting , logging and cors

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,19 +3,57 @@
  * This is only a minimal backend to get started.
  */
 
-import express from 'express';
-import * as path from 'path';
+import express from "express";
+import cors from "cors";
+import proxy from "express-http-proxy";
+import morgan from "morgan";
+import rateLimit, { ipKeyGenerator } from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
+import axios from "axios";
+import cookieParser from "cookie-parser";
+
+import * as path from "path";
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api-gateway!' });
+app.use(morgan("dev"));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(cookieParser());
+app.set("trust proxy", 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req:any) => (req.user? 1000:100), // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req: any) => {
+    const safeIp = ipKeyGenerator(req); // normalized IP (handles IPv6 safely)
+    return req.user ? `${safeIp}-${req.user.id}` : safeIp;
+  },
 });
+
+app.use(limiter);
+
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+
+app.get("/gateway-health", (req, res) => {
+  res.send({ message: "Welcome to api-gateway!" });
+});
+
+app.use("/",proxy("http://localhost:6001"));
 
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
-server.on('error', console.error);
+server.on("error", console.error);
